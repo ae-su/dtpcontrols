5b636a48b35cfdc05774f1a43d0233cf52bc27cb
Joeal Subash <joeal.subash@cern.ch>
2022-03-22 15:46:45 +0000

    Fixed CentralRouterInterfaceNodePyBind formatting

--- pybindsrc/CentralRouterInterfaceNodePyBind.cpp
+++ pybindsrc/CentralRouterInterfaceNodePyBind.cpp
@@ -13,20 +13,20 @@
 namespace py = pybind11;
 
 namespace dunedaq {
-namespace dtpcontrols {
-namespace python {
+  namespace dtpcontrols {
+    namespace python {
 
-void register_central_router_interface_node(py::module& m){ 
+      void register_central_router_interface_node(py::module& m){ 
 
-    m.doc() = "c++ implementation of dtpcontrols CentralRouterInterface python modules"; // optional module docstring
+	m.doc() = "c++ implementation of dtpcontrols CentralRouterInterface python modules"; // optional module docstring
 
-     py::class_<dtpcontrols::CentralRouterInterfaceNode, uhal::Node>(m, "CentralRouterInterfaceNode")
-       .def(py::init<const uhal::Node&>())
-       .def("enable_central_router_interface", &dtpcontrols::CentralRouterInterfaceNode::enable_central_router_interface,
-	    py::arg("dispatch") = true)
-       .def("drop_empty", &dtpcontrols::CentralRouterInterfaceNode::drop_empty, py::arg("dispatch") = true);
-}
+	py::class_<dtpcontrols::CentralRouterInterfaceNode, uhal::Node>(m, "CentralRouterInterfaceNode")
+	  .def(py::init<const uhal::Node&>())
+	  .def("enable_central_router_interface", &dtpcontrols::CentralRouterInterfaceNode::enable_central_router_interface,
+	       py::arg("dispatch") = true)
+	  .def("drop_empty", &dtpcontrols::CentralRouterInterfaceNode::drop_empty, py::arg("dispatch") = true);
+      }
 
-} // namespace python
-} // namespace timing
+    } // namespace python
+  } // namespace timing
 } // namespace dunedaq
